28e0709140bdfb374a3aa26d8fce989c1f3a7dff
nickysantano <56449770+nickysantano@users.noreply.github.com>
2022-06-21 15:59:02 +0700

    onboarding done

--- mimoni/mimoni/mimoniApp.swift
+++ mimoni/mimoni/mimoniApp.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 @main
 struct mimoniApp: App {
-    let persistenceController = PersistenceController.shared
+    @AppStorage("isOnboarding") var isOnboarding = true
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            if isOnboarding{
+                OnboardingView()
+            }else{
+                ContentView()
+            }
+            
         }
     }
 }
