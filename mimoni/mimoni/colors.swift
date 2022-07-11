d6b1364c8e2ea786587e5a2e360292ac3803bcf5
Arnold Sidiprasetija <arnolds1809@gmail.com>
2022-07-11 15:49:42 +0700

    add: app color

---
+++ mimoni/mimoni/colors.swift
@@ -0,0 +1,19 @@
+//
+//  colors.swift
+//  mimoni
+//
+//  Created by Arnold Sidiprasetija on 11/07/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let interactiveColor = Color("FFB700")
+    
+    // Ring Chart
+    static let green =  Color("3A8D87")
+    static let purple =  Color("5262B5")
+    static let red = Color("FF5547")
+    static let yellow =  Color("FFCC1F")
+    
+}
