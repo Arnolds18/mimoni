ab7980dd2489b7b24a429889e568a487dcef9271
Ardian-Kurniawan <56443318+Ardiankls@users.noreply.github.com>
2022-07-13 15:32:08 +0700

    refactor move TimelineProvider's Struct from Moneh to a new file

--- mimoni/MonehWidget/MonehWidget.swift
+++ mimoni/MonehWidget/MonehWidget.swift
@@ -9,31 +9,7 @@ import WidgetKit
 import SwiftUI
 import Intents
 
-struct Provider: IntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
-    }
-    
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
-        completion(entry)
-    }
-    
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-        
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-        
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
-}
+
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
@@ -72,7 +48,7 @@ struct MonehWidgetEntryView : View {
 
 @main
 struct MonehWidget: Widget {
-    let kind: String = "MonehWidget"
+    let kind: String = "MonehWidgetSmall"
     
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
