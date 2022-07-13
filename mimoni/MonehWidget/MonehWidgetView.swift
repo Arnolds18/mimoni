cf7e7b0fd665d09f8b49438ed8c96fa3bd132d8b
Ardian-Kurniawan <56443318+Ardiankls@users.noreply.github.com>
2022-07-13 16:51:30 +0700

    test switch size widget

--- mimoni/MonehWidget/MonehWidgetView.swift
+++ mimoni/MonehWidget/MonehWidgetView.swift
@@ -13,7 +13,8 @@ struct MonehWidgetEntryView : View {
     var entry: Provider.Entry
     var body: some View {
         switch family{
-        case .systemSmall:
+        case .systemMedium:
+//            Text("Widget Small")
             VStack(alignment: .leading){
                 //            Text(entry.date, style: .time)
                 Spacer()
@@ -34,13 +35,34 @@ struct MonehWidgetEntryView : View {
                 })
                 Spacer()
             }
-        case .systemMedium:
-            Text("Widget Medium")
+        case .systemSmall:
+            Text("Test Widget Medium")
+//            VStack(alignment: .leading){
+//                        //            Text(entry.date, style: .time)
+//                        Spacer()
+//                        Text("Sisa Uang:").font(.caption).frame(alignment: .leading)
+//                        Spacer()
+//                        Text("Rp. 2,000,000,-")
+//                        Spacer()
+//                        Button(action: {""
+//                        }, label: {
+//                            Text("+Add Expenses")
+//                                .font(.caption)
+//                                .fontWeight(.semibold)
+//                                .foregroundColor(.black)
+//                                .padding()
+//                                .background(Color.yellow
+//                                    .cornerRadius (8)
+//                                )
+//                        })
+//                        Spacer()
+//                    }
         default:
             Text("not implemented")
         }
     }
 }
+
 struct MonehWidget_Previews: PreviewProvider {
     static var previews: some View {
         Group{
