99999c96d4310a292b10cc8e8b6978c9bc89dca0
Ardian-Kurniawan <56443318+Ardiankls@users.noreply.github.com>
2022-07-14 15:39:33 +0700

    style make layout and ring for medium widget

--- mimoni/MonehWidget/MonehWidgetView.swift
+++ mimoni/MonehWidget/MonehWidgetView.swift
@@ -14,71 +14,141 @@ struct MonehWidgetEntryView : View {
     var body: some View {
         switch family{
         case .systemSmall:
-//            Text("Test Widget Small")
-                        VStack(alignment: .leading){
-                            //            Text(entry.date, style: .time)
-                            Spacer()
-                            Text("Sisa Uang:").font(.caption).frame(alignment: .leading)
-                            Spacer()
-                            Text("Rp. 2,000,000,-")
-                            Spacer()
-                            Button(action: {""
-                            }, label: {
-                                Text("+Add Expenses")
-                                    .font(.caption)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.black)
-                                    .padding()
-                                    .background(Color.yellow
-                                        .cornerRadius (8)
-                                    )
-                            })
-                            Spacer()
-                        }
+            //            Text("Test Widget Small")
+            VStack(alignment: .leading){
+                //            Text(entry.date, style: .time)
+                Spacer()
+                Text("Sisa Uang:").font(.caption).frame(alignment: .leading)
+                Spacer()
+                Text("Rp. 2,000,000,-")
+                Spacer()
+                Button(action: {""
+                }, label: {
+                    Text("+Add Expenses")
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(Color.yellow
+                            .cornerRadius (8)
+                        )
+                })
+                Spacer()
+            }
         case .systemMedium:
-            Text("Test Widget Medium")
-//                        VStack(alignment: .leading){
-//                                    //            Text(entry.date, style: .time)
-//                                    Spacer()
-//                                    Text("Sisa Uang:").font(.caption).frame(alignment: .leading)
-//                                    Spacer()
-//                                    Text("Rp. 2,000,000,-")
-//                                    Spacer()
-//                                    Button(action: {""
-//                                    }, label: {
-//                                        Text("+Add Expenses")
-//                                            .font(.caption)
-//                                            .fontWeight(.semibold)
-//                                            .foregroundColor(.black)
-//                                            .padding()
-//                                            .background(Color.yellow
-//                                                .cornerRadius (8)
-//                                            )
-//                                    })
-//                                    Spacer()
-//                                }
+            //            Text("Test Widget Medium")
+            VStack{
+                //            Text(entry.date, style: .time)
+                HStack{
+                    VStack(alignment: .leading){
+                        Text("Sisa Uang:").font(.caption).frame(alignment: .leading)
+                        Text("Rp. 2,000,000,-")
+                    }
+                    Spacer()
+                    Button(action: {""
+                    }, label: {
+                        Circle()
+                            .fill(Color.yellow)
+                            .frame(width: 40, height: 40)
+                            .overlay(
+                                Image(systemName: "plus")
+                                    .foregroundColor(.white)
+                            )
+                    })
+                }
+                Spacer()
+                HStack{
+                    VStack{
+                        ZStack(alignment: .leading){
+                            Circle() //circle yang belakang
+                                .trim(from: 0, to: 1)
+                                .stroke(Color.yellow.opacity(0.25), lineWidth: 8)
+                                .frame(width: (UIScreen.main.bounds.width - 310) / 2, height: (UIScreen.main.bounds.width - 310) / 2)
+                            
+                            
+                            Circle() //circle depan buat tau datanya berapa
+                                .trim(from: 0.4, to: 0.75) //nanti to nya dari batas dari budget yang di set - data total pengeluaran
+                                .stroke(Color.yellow, lineWidth: 8)
+                                .frame(width: (UIScreen.main.bounds.width - 310) / 2, height: (UIScreen.main.bounds.width - 310) / 2)
+                        }.padding(EdgeInsets(top: 6, leading: 0, bottom: 5, trailing: 0))
+                        Text("Kebutuhan")
+                            .font(.system(size: 8).weight(.semibold))
+                    }
+                    Spacer()
+                    VStack{
+                        ZStack(alignment: .leading){
+                            Circle() //circle yang belakang
+                                .trim(from: 0, to: 1)
+                                .stroke(Color.red.opacity(0.25), lineWidth: 8)
+                                .frame(width: (UIScreen.main.bounds.width - 310) / 2, height: (UIScreen.main.bounds.width - 310) / 2)
+                            
+                            
+                            Circle() //circle depan buat tau datanya berapa
+                                .trim(from: 0.2, to: 0.75) //nanti to nya dari batas dari budget yang di set - data total pengeluaran
+                                .stroke(Color.red, lineWidth: 8)
+                                .frame(width: (UIScreen.main.bounds.width - 310) / 2, height: (UIScreen.main.bounds.width - 310) / 2)
+                        }.padding(EdgeInsets(top: 6, leading: 0, bottom: 5, trailing: 0))
+                        Text("Keinginan")
+                            .font(.system(size: 8).weight(.semibold))
+                    }
+                    Spacer()
+                    VStack{
+                        ZStack(alignment: .leading){
+                            Circle() //circle yang belakang
+                                .trim(from: 0, to: 1)
+                                .stroke(Color.green.opacity(0.25), lineWidth: 8)
+                                .frame(width: (UIScreen.main.bounds.width - 310) / 2, height: (UIScreen.main.bounds.width - 310) / 2)
+                            
+                            
+                            Circle() //circle depan buat tau datanya berapa
+                                .trim(from: 0.090, to: 0.75) //nanti to nya dari batas dari budget yang di set - data total pengeluaran
+                                .stroke(Color.green, lineWidth: 8)
+                                .frame(width: (UIScreen.main.bounds.width - 310) / 2, height: (UIScreen.main.bounds.width - 310) / 2)
+                        }.padding(EdgeInsets(top: 6, leading: 0, bottom: 5, trailing: 0))
+                        Text("Tabungan")
+                            .font(.system(size: 8).weight(.semibold))
+                    }
+                    Spacer()
+                    VStack{
+                        ZStack(alignment: .leading){
+                            Circle() //circle yang belakang
+                                .trim(from: 0, to: 1)
+                                .stroke(Color.purple.opacity(0.25), lineWidth: 8)
+                                .frame(width: (UIScreen.main.bounds.width - 310) / 2, height: (UIScreen.main.bounds.width - 310) / 2)
+                            
+                            
+                            Circle() //circle depan buat tau datanya berapa
+                                .trim(from: 0, to: 0.75) //nanti to nya dari batas dari budget yang di set - data total pengeluaran
+                                .stroke(Color.purple, lineWidth: 8)
+                                .frame(width: (UIScreen.main.bounds.width - 310) / 2, height: (UIScreen.main.bounds.width - 310) / 2)
+                        }.padding(EdgeInsets(top: 6, leading: 0, bottom: 5, trailing: 0))
+                        Text("Dana darurat")
+                            .font(.system(size: 8).weight(.semibold))
+                    }
+                }
+            }.padding(25)
         default:
             Text("None")
-//            VStack(alignment: .leading){
-//                //            Text(entry.date, style: .time)
-//                Spacer()
-//                Text("Sisa Uang:").font(.caption).frame(alignment: .leading)
-//                Spacer()
-//                Text("Rp. 2,000,000,-")
-//                Spacer()
-//                Button(action: {""
-//                }, label: {
-//                    Text("+Add Expenses")
-//                        .font(.caption)
-//                        .fontWeight(.semibold)
-//                        .foregroundColor(.black)
-//                        .padding()
-//                        .background(Color.yellow
-//                            .cornerRadius (8)
-//                        )
-//                })
-//                Spacer()
-//            }
+            //            VStack(alignment: .leading){
+            //                //            Text(entry.date, style: .time)
+            //                Spacer()
+            //                Text("Sisa Uang:").font(.caption).frame(alignment: .leading)
+            //                Spacer()
+            //                Text("Rp. 2,000,000,-")
+            //                Spacer()
+            //                Button(action: {""
+            //                }, label: {
+            //                    Text("+Add Expenses")
+            //                        .font(.caption)
+            //                        .fontWeight(.semibold)
+            //                        .foregroundColor(.black)
+            //                        .padding()
+            //                        .background(Color.yellow
+            //                            .cornerRadius (8)
+            //                        )
+            //                })
+            //                Spacer()
+            //            }
         }
     }
 }
